b1791738aa78ba8237e65742e5ff9298d44ac5d2
Joshua Hoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2022-03-30 16:41:32 +0200

    One more bugfix

--- src/scraper/provider.ts
+++ src/scraper/provider.ts
@@ -28,7 +28,7 @@ export abstract class Provider {
 
   async queryDistrict(zipCode: string): Promise<string | null> {
     try {
-      const response = await fetch(`api.zippopotam.us/de/${zipCode}`).then(response => response.json()) as any
+      const response = await fetch(`https://api.zippopotam.us/de/${zipCode}`).then(response => response.json()) as any
       return response.places[response.places.length - 1]["place name"] ?? null
     } catch (error) {
       console.log(error)
